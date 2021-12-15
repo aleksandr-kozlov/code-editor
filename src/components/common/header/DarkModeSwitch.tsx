64290241adc3b5be0c1ac514dc0ac25e2ef69474
Aleksandr Kozlov <aleksandr_kozlov_98@inbox.ru>
2021-12-15 16:54:46 +0300

    implemented dark mode switch component

---
+++ src/components/common/header/DarkModeSwitch.tsx
@@ -0,0 +1,22 @@
+import { Switch } from '@mui/material';
+import React from 'react';
+import {useAppDispatch, useAppSelector} from "../../../store/hooks";
+import {toggleDarkMode} from "../../../store/slices/dark-mode";
+import DarkModeIcon from '@mui/icons-material/Brightness2';
+
+/**
+ * Dark mode switch component
+ */
+function DarkModeSwitch() {
+    const isDarkModeEnabled = useAppSelector(state => state.darkMode);
+    const dispatch = useAppDispatch();
+
+    const onChange = () => dispatch(toggleDarkMode());
+
+    return <>
+        <DarkModeIcon/>
+        <Switch color={'default'} onChange={onChange} checked={isDarkModeEnabled}/>
+    </>
+}
+
+export default DarkModeSwitch;
