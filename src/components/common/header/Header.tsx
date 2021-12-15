69aa94bda77a006798b1a86c9ccd2a4f89315979
Aleksandr Kozlov <aleksandr_kozlov_98@inbox.ru>
2021-12-15 16:59:43 +0300

    moved header's components into separate directory

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -2,11 +2,11 @@ import { AppBar, Toolbar, Typography } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import {Link} from "react-router-dom";
 import {paths} from "../../../routes/paths";
-import SignInButton from "./SignInButton";
-import SignOutButton from "./SignOutButton";
-import CodeEditorButton from "./CodeEditorButton";
+import SignInButton from "./components/SignInButton";
+import SignOutButton from "./components/SignOutButton";
+import CodeEditorButton from "./components/CodeEditorButton";
 import {useAuth0} from "@auth0/auth0-react";
-import DarkModeSwitch from "./DarkModeSwitch";
+import DarkModeSwitch from "./components/DarkModeSwitch";
 
 const StyledLink = styled(Link)(({ theme }) => ({
     textDecoration: 'none',
