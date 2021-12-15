0430aaf566eeef2556ac998de178eef938c74fa5
Aleksandr Kozlov <aleksandr_kozlov_98@inbox.ru>
2021-12-15 14:53:51 +0300

    added base layout component

---
+++ src/components/common/layouts/BaseLayout.tsx
@@ -0,0 +1,26 @@
+import React, { PropsWithChildren } from 'react';
+import {styled} from "@mui/material/styles";
+import Header from "../header/Header";
+
+const LayoutContainer = styled('div')({
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+});
+
+const Page = styled('div')({
+    height: '100%',
+})
+
+function BaseLayout(props: PropsWithChildren<{}>) {
+    return (
+        <LayoutContainer>
+            <Header/>
+            <Page>
+                {props.children}
+            </Page>
+        </LayoutContainer>
+    );
+}
+
+export default BaseLayout;
