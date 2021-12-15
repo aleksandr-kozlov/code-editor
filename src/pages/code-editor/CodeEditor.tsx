34ceead18e86d6e471ad0557b749ae28b0d233e5
Aleksandr Kozlov <aleksandr_kozlov_98@inbox.ru>
2021-12-15 15:03:02 +0300

    added code-editor page (mocked)

---
+++ src/pages/code-editor/CodeEditor.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from "../../components/common/layouts/BaseLayout";
+
+function CodeEditor() {
+    return (
+        <Layout>
+            Code editor space
+        </Layout>
+    );
+}
+
+export default CodeEditor;
