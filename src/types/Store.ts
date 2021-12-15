e7a14a965e2e56c218293a55e197a332ef108c7c
Aleksandr Kozlov <aleksandr_kozlov_98@inbox.ru>
2021-12-15 17:17:54 +0300

    implemented files redux slice

--- src/types/Store.ts
+++ src/types/Store.ts
@@ -1,5 +1,11 @@
 import store from '../store';
 
+/**
+ * Root state type
+ */
 export type RootState = ReturnType<typeof store.getState>
 
+/**
+ * App dispatch type
+ */
 export type AppDispatch = typeof store.dispatch;
