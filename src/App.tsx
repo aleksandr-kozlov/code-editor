c03f1f0d631614a9c20a1a2aad1385897403157d
Aleksandr Kozlov <aleksandr_kozlov_98@inbox.ru>
2021-12-15 16:57:30 +0300

    moved providers to app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
-import './App.css';
+import { Suspense } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import AuthProvider from "./auth/AuthProvider";
+import CustomThemeProvider from "./theme/CustomThemeProvider";
+import Routes from "./routes/Routes";
+import Loading from "./components/common/Loading/Loading";
 
 function App() {
   return (
-      <div>Hello world</div>
+      <Router>
+        <AuthProvider>
+          <CustomThemeProvider>
+              <Suspense fallback={<Loading/>}>
+                <Routes/>
+              </Suspense>
+          </CustomThemeProvider>
+        </AuthProvider>
+      </Router>
   );
 }
 
