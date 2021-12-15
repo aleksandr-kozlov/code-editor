5a989c3d98ba14b432da35eb8269b717ad24b828
Aleksandr Kozlov <aleksandr_kozlov_98@inbox.ru>
2021-12-15 16:32:30 +0300

    fix import and export at redux

--- src/store/index.ts
+++ src/store/index.ts
@@ -17,7 +17,4 @@ const store = configureStore({
 
 export const persistor = persistStore(store);
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
-
 export default store;
--- src/store/root-reducer.ts
+++ src/store/root-reducer.ts
@@ -1,5 +1,5 @@
 import {combineReducers} from "@reduxjs/toolkit";
-import darkModeReducer from "./slices/dark-mode";
+import {darkModeReducer} from "./slices/dark-mode";
 
 const rootReducer = combineReducers({
     darkMode: darkModeReducer,
