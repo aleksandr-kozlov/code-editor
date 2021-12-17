11c8957c95c286a2e56a4196bd632c48d29483ee
Aleksandr Kozlov <aleksandr_kozlov_98@inbox.ru>
2021-12-17 22:59:57 +0300

    added files reducer to root reducer

--- src/store/root-reducer.ts
+++ src/store/root-reducer.ts
@@ -1,8 +1,10 @@
 import {combineReducers} from "@reduxjs/toolkit";
 import {darkModeReducer} from "./slices/dark-mode";
+import { filesReducer } from "./slices/files";
 
 const rootReducer = combineReducers({
     darkMode: darkModeReducer,
+    files: filesReducer,
 })
 
 export default rootReducer;
